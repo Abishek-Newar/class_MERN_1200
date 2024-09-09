f41ae7e1577fbe475d686e601dcda21136aef8d8
abishejk1872 <abisheknewar15@gmail.com>
2024-09-09 12:56:48 +0530

    added class

---
+++ mon0909/script.js
@@ -0,0 +1,48 @@
+console.log("hi there")
+
+//variables in js
+
+//#var let const
+var x = 10
+let y = 10
+const z = 10
+
+//#data types
+
+//Number: 1,2,2.1
+//String: 
+//Boolean: true/false, 0/1
+//objects
+//arays
+//symbols
+
+//#operators
+//Arithmetic : +, - , * , / , %
+//Assignment: = , +=, -=, *=, /= , %=                x = 10;   x+=1, x = 11
+//Logical:  OR ( || ), AND ( &&) , NOT( ! )
+//Relational/comparison :    ===, == ,!=, !==,>,<,>=,<=               ===,==          
+
+
+//# conditionals
+// if else
+ if(condition){
+    //code
+ }else{
+    //code
+ }
+
+ //if else if else
+ if(condtion){
+    //code
+ }else if(condition){
+    //code
+ }else{
+    //code
+ }
+
+ // write a program 
+ // take a number and print according to the folloeing
+ // A  90-100
+ // B  80-89
+//  C  70-79
+//  D  0-69
