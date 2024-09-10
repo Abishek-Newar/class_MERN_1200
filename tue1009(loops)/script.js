2ce839842b20e6a952602c6c18421f73eefb0a39
abishejk1872 <abisheknewar15@gmail.com>
2024-09-10 12:45:50 +0530

    loops done

--- tue1009(loops)/script.js
+++ tue1009(loops)/script.js
@@ -8,17 +8,43 @@ for(let i = 0;i<10;i++){ // 0 1 2 3 4 5 6 7 8 9
 
 //while loop
 
-while(condition){
+// while(condition){
 
-}
+// }
 
 //infinite loop
-while(true){
+// while(true){
 
-}
+// }
+//program to print 1 to 10
+// let a =1;
+// while(a<=10){
+//     console.log(a)
+//     a+= 1;
+// }
 
-let a =1;
-while(a<=10){
+//program to print 10 to 1
+
+// let a = 10;
+// while(a>0){
+//     console.log(a)
+//     a-= 1;
+// }
+
+// do while loop
+
+do{
+    //code
+}while(condition)
+
+let a  = 1;
+do{
     console.log(a)
-    a+= 1;
-}
+    a++;
+}while(a<=10)
+
+//take user input
+
+var a = parseInt(prompt("enter a number"))
+
+//write a program to print the table of a number given by user
