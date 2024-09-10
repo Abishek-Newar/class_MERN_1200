364401ed6b44c0f2d6185e3c08455d029ee61d50
abishejk1872 <abisheknewar15@gmail.com>
2024-09-10 12:32:42 +0530

    added for and while loop

---
+++ tue1009(loops)/script.js
@@ -0,0 +1,24 @@
+//loops
+
+// for   while     do while 
+//for loop
+for(let i = 0;i<10;i++){ // 0 1 2 3 4 5 6 7 8 9
+    //code
+}  
+
+//while loop
+
+while(condition){
+
+}
+
+//infinite loop
+while(true){
+
+}
+
+let a =1;
+while(a<=10){
+    console.log(a)
+    a+= 1;
+}
