2c16131692c2f6b816cd5e4c9ac52325f5848f44
abishejk1872 <abisheknewar15@gmail.com>
2024-10-23 13:36:09 +0530

    changs

--- FullStackTodoApplication-2709/frontend/src/components/Navbar.jsx
+++ FullStackTodoApplication-2709/frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
    <nav className='flex items-center justify-center  bg-[#45D1FD] h-16'>
     <ul className='flex items-center gap-4 text-xl font-bold text-white cursor-pointer'>
-      <Link to="/"><li>Home</li></Link>
+      <Link to="/home"><li>Home</li></Link>
       <Link to="/add"><li>Add</li></Link>
     </ul>
    </nav>
--- FullStackTodoApplication-2709/frontend/src/components/Signin.jsx
+++ FullStackTodoApplication-2709/frontend/src/components/Signin.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom'
+import {LabeledInput} from './Signup'
 const Signin = ({authType,setAuthType}) => {
+    const navigate = useNavigate()
+    const [signupdata,setSignupdata] = React.useState({
+        email: "",
+        password: ""
+    })
+    function handleChange(type,e){
+        setSignupdata((prev)=>({
+            ...prev,
+            [type]: e.target.value
+        }))
+    }
+
+    async function handleSubmit(){
+        try{
+            const response  = await fetch("http://localhost:3000/login",{
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    email: signupdata.email,
+                    password: signupdata.password
+                })
+            })
+            const data = await response.json()
+            localStorage.setItem("token",data.token)
+            navigate("/home")
+        }catch(e){
+            alert("error while signinup")
+        }
+    }
   return (
-    <div>Signin</div>
+    <div className='min-h-screen flex items-center justify-center'>
+        <div className='w-96 h-auto border rounded-md p-6'>
+            <div>
+                <LabeledInput type="email" placeholder="abi@gmal.com" id="email" name="Email" onChange={(e)=>handleChange("email",e)} />
+                <LabeledInput type="password" placeholder="" id="password" name="Password" onChange={(e)=>handleChange("password",e)} />
+            </div>
+            <button onClick={handleSubmit}>Signin</button>
+            <p>Don't Have a Account? <span className='cursor-pointer' onClick={()=>setAuthType("signup")}>Signup</span></p>
+        </div>
+    </div>
   )
 }
 
