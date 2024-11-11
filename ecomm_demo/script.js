2bc7333d7638197bb41d6b9207f6159a8d8b493f
abishejk1872 <abisheknewar15@gmail.com>
2024-11-11 15:55:15 +0530

    ecomm-demo comp

--- ecomm_demo/script.js
+++ ecomm_demo/script.js
@@ -19,3 +19,26 @@ async function LatestData(){
     }
 }
 LatestData()
+const allPro = document.getElementById("allProductShow")
+async function allProductData(){
+    const res = await fetch('https://dummyjson.com/products')
+    const data = await res.json()
+    console.log(data)
+    for(let i = 0;i<data.products.length;i++){
+        const card = document.createElement("div")
+        const image = document.createElement("img")
+        const h1 = document.createElement("h1")
+        const price = document.createElement("p")
+        image.src = data.products[i].thumbnail
+        h1.textContent = data.products[i].title
+        price.textContent = data.products[i].price
+        card.append(image)
+        card.append(h1)
+        card.append(price)
+        card.classList.add("card")
+        allPro.append(card)
+    }
+}
+allProductData()
+
+ 
