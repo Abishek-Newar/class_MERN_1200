73617f610eae704f0fb3d3ce0386893b55ccc13e
abishejk1872 <abisheknewar15@gmail.com>
2024-12-06 14:53:31 +0530

    databse conenctivity

--- nodejs_demo/index.js
+++ nodejs_demo/index.js
@@ -1,15 +1,43 @@
 //http: helps create a http server
-//fs: file system (read or write files)
-//
+//fs: file system ( file read and write)
+//path: 
+//os: operating system
+//events: event handling
+
+
 const http = require("http")
+
+//import mongodb and mongoclient
+const {MongoClient} = require("mongodb")
+
 
 const server = http.createServer((req,res)=>{
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('Welcome to MERN class');
+    res.write('Welcome to this page!');
     res.end();
 })
 
+//url of database
+const uri = "mongodb+srv://abishekitm:abishek123@cluster0.syapi.mongodb.net/"
+
+//creating a mongo client
+const client  = new MongoClient(uri)
+
+function dbConnection(){
+    //connecting the client and handling promise
+    client.connect()
+    .then(()=>{
+        console.log("mongodb connected")
+    })
+    .catch(()=>{
+        console.log("error while conencting")
+    })
+}
+
+dbConnection()
+
+
 
 server.listen(3000,()=>{
-    console.log("server connected")
+    console.log("port connected")
 })
