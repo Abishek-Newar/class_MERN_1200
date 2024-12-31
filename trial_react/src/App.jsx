b6bd2a9ed1b76171f106154ba3a4a32cae69f37d
abishejk1872 <abisheknewar15@gmail.com>
2024-12-31 15:05:34 +0530

    react started

---
+++ trial_react/src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar"
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+    hi there
+    <h1 className="header1">React Project</h1>
+
+    
+    </>
+  )
+}
+
+export default App
