337403dd58aefca5e5e3d79d9a4b9882bc9575e6
abishejk1872 <abisheknewar15@gmail.com>
2025-01-14 13:05:43 +0530

    changes

---
+++ react_hooks/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, { memo } from 'react'
+
+const Navbar = ({data,setData}) => {
+    console.log("hii there")
+  return (
+    <div>
+        navbar
+    </div>
+  )
+}
+
+export default memo(Navbar)
